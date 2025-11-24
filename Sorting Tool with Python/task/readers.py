377c2a9307a1372797074681ad121da25d7e8c24
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-24 19:47:13 +0100

    Enhance analysis methods to include statistics for longest line and longest word

--- Sorting Tool with Python/task/readers.py
+++ Sorting Tool with Python/task/readers.py
@@ -1,6 +1,7 @@
 """This module provides classes for reading different types of data from input."""
 from abc import ABC, abstractmethod
 from collections import Counter
+from math import floor
 
 class Reader(ABC):
     """Abstract base class for data readers."""
@@ -27,13 +28,13 @@ class LongReader(Reader):
         - percentage of the greatest value occurrences
         """
 
-        num_of_ints = len(self.data)
+        total = len(self.data)
         greatest = max(self.data)
         counts = Counter(self.data)
         num_of_greatest = counts[greatest]
-        percentage = (num_of_greatest / num_of_ints) * 100
+        percentage = floor((num_of_greatest / total) * 100)
 
-        print(f'Total numbers: {num_of_ints}.')
+        print(f'Total numbers: {total}.')
         print(f'The greatest number: {greatest} ({num_of_greatest} time(s), {percentage:.0f}%).')
 
     def __init__(self):
@@ -55,7 +56,24 @@ class LineReader(Reader):
     """Reader for lines of text."""
 
     def analyze_data(self) -> None:
-        raise NotImplementedError
+        """Analyze collected lines of text and print results.
+
+        - total number of lines
+        - longest line
+        - number of occurrences of the longest line
+        - percentage of the longest line occurrences
+        """
+
+        total = len(self.data)
+        longest_line = max(self.data, key=len)
+        counts = Counter(self.data)
+        num_of_longest = counts[longest_line]
+        percentage = floor((num_of_longest / total) * 100)
+
+        print(f'Total lines: {total}.')
+        print('The longest line:')
+        print(longest_line)
+        print(f'({num_of_longest} times(s), {percentage:.0f}%).')
 
     def __init__(self):
         self.data: list[str] = []
@@ -75,7 +93,22 @@ class WordReader(Reader):
     """Reader for words."""
 
     def analyze_data(self) -> None:
-        raise NotImplementedError
+        """Analyze collected words and print results.
+
+        - total number of words
+        - longest word
+        - number of occurrences of the longest word
+        - percentage of the longest word occurrences
+        """
+
+        total = len(self.data)
+        longest_word = max(self.data, key=len)
+        counts = Counter(self.data)
+        num_of_longest = counts[longest_word]
+        percentage = floor((num_of_longest / total) * 100)
+
+        print(f'Total words: {total}.')
+        print(f'The longest word: {longest_word} ({num_of_longest} time(s), {percentage:.0f}%).')
 
     def __init__(self):
         self.data: list[str] = []
