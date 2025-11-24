57859c2d7928ceee2405993f63c34a13479a692e
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-24 19:20:25 +0100

    Add LineReader and WordReader

--- Sorting Tool with Python/task/readers.py
+++ Sorting Tool with Python/task/readers.py
@@ -27,3 +27,39 @@ class LongReader(Reader):
                 self.data.extend(numbers)
             except EOFError:
                 break
+
+class LineReader(Reader):
+    """Reader for lines of text."""
+
+    def __init__(self):
+        self.data: list[str] = []
+
+    def collect_data(self) -> None:
+        """Collect lines of text from input."""
+
+        while True:
+            try:
+                line = input()
+                self.data.append(line)
+            except EOFError:
+                break
+
+class WordReader(Reader):
+    """Reader for words."""
+
+    def __init__(self):
+        self.data: list[str] = []
+
+    def collect_data(self) -> None:
+        """Collect words from input.
+
+        Word is any number of characters separated by whitespace.
+        """
+
+        while True:
+            try:
+                line = input()
+                words = line.split()
+                self.data.extend(words)
+            except EOFError:
+                break
