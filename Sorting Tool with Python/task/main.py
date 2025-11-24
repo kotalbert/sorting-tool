421c4055b9cb4461f75371cc1b246cce6cd640a8
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-24 20:15:13 +0100

    Add sortIntegers argument

--- Sorting Tool with Python/task/main.py
+++ Sorting Tool with Python/task/main.py
@@ -6,9 +6,14 @@ from readers import LongReader, WordReader, LineReader
 def main():
     parser = ArgumentParser()
     parser.add_argument('-dataType', type=str, default='long', choices=['long', 'word', 'line'], )
+    parser.add_argument('-sortIntegers', action='store_true', default=False)
     args = parser.parse_args()
 
     reader = None
+    # guard against reading words or lines when sorting integers
+    if args.sortIntegers:
+        args.dataType = 'long'
+
     match args.dataType:
         case 'long':
             reader = LongReader()
@@ -20,7 +25,16 @@ def main():
             reader = LongReader()
 
     reader.collect_data()
-    reader.analyze_data()
+
+    if not args.sortIntegers:
+        reader.analyze_data()
+    else:
+
+        num_of_ints = len(reader.data)
+        reader.data.sort()
+        sorted_str = ' '.join(map(str, reader.data))
+        print(f'Total numbers: {num_of_ints}.')
+        print(f'Sorted data: {sorted_str}')
 
 
 if __name__ == '__main__':
